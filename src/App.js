bf5fed86093c4894f6dcd29686191e8baa8650ad
Kimi <lys.kimi@gms.tku.edu.tw>
2022-01-21 17:29:12 +0800

    Modified: finish nav/tab part

--- src/App.js
+++ src/App.js
@@ -4,48 +4,26 @@ import PageTitle from './component/PageTitle';
 function App() {
   return (
     <div class="container max-w-fit m-0">
-      <PageTitle>To Do List</PageTitle>
-      {/* <div class="nav bg-blue w-full h-76 fix"></div> */}
+      {/* <PageTitle class="text-2xl font-bold text-gray-500 text-center leading-8 my-5">To Do List</PageTitle> */}
 
       {/* tab section start  */}
-      <nav class="justify-center flex my-0 mx-auto bg-blue w-full">
-        <ul class="tabs list-none items-center mx-auto" >
-          <li class="float-left block py-3 px-10">
-            <label
-              for="tab1"
-              rel="#tab-content1"
-              class="tab1-1 block relative font-medium active text-base  cursor-pointer w-full bg-blue text-white active:border-b-5 active:border-solid active:border-gray-dark"
-            >
-              My Tasks
-            </label>
-          </li>
-          <li class="float-left block py-3 px-10">
-            <label
-              for="tab2"
-              rel="#tab-content2"
-              class="tab1-2 font-normal text-base text-gray-dark block w-full cursor-pointer bg-blue active:text-white"
-            >
-              In Progress
-            </label>
-          </li>
-          <li class="float-left block py-3 px-10">
-            <label
-              for="tab3"
-              rel="#tab-content3"
-              class="tab1-3 font-normal text-base text-gray-dark block w-full cursor-pointer bg-blue active:text-white"
-            >
-              Completed
-            </label>
-          </li>
-        </ul>
-      </nav>
+      <input type="radio" name="tab" id="content1" class="hidden" checked></input>
+      <input type="radio" name="tab" id="content2" class="hidden"></input>
+      <input type="radio" name="tab" id="content3" class="hidden"></input>
 
-      <div class="tab-content-container flex flex-wrap">
-        <div
-          id="tab-content1"
-          class="tab-content opacity-100  w-full bg-gray-light overflow-hidden active:opacity-100"
-          aria-hidden="false"
-        >
+      <div class="nav flex justify-center items-center mx-auto bg-blue h-16 w-full">
+        <label for="content1">
+          <span class="font-normal text-base  cursor-pointer text-blue-dark bg-blue px-10 py-5 transition">My Tasks</span>
+        </label>
+        <label for="content2">
+          <span class="font-normal text-base  cursor-pointer text-blue-dark bg-blue px-10 py-5 transition">In Progress</span>
+        </label>
+        <label for="content3">
+          <span class="font-normal text-base  cursor-pointer text-blue-dark bg-blue px-10 py-5 transition">Completed</span>
+        </label>
+      </div>
+
+      <div class="tab-content1 hidden w-full bg-gray-lighter overflow-hidden">
         <div class="my-10 mx-40 flex justify-center">
           <p>
             "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
@@ -56,38 +34,22 @@ function App() {
             pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
             culpa qui officia deserunt mollit anim id est laborum."
           </p>
-          </div>
         </div>
-
-        <div
-          id="tab-content2"
-          class="tab-content hidden w-full bg-gray-light overflow-hidden active:opacity-100"
-          aria-hidden="true"
-        >
-        <div class="mx-40 flex justify-center">
+      </div>
+      <div class="tab-content2 hidden w-full bg-gray-lighter overflow-hidden">
+        <div class="my-10 mx-40 flex justify-center">
           <p>
             "Sed ut perspiciatis unde omnis iste natus error sit voluptatem
             accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
             quae ab illo inventore veritatis et quasi architecto beatae vitae
             dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
             aspernatur aut odit aut fugit, sed quia consequuntur magni dolores
-            eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est,
-            qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit,
-            sed quia non numquam eius modi tempora incidunt ut labore et dolore
-            magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis
-            nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut
-            aliquid ex ea commodi consequatur? Quis autem vel eum iure
-            reprehenderit qui in ea voluptate velit esse quam nihil molestiae
-            consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla?
+            eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est
           </p>
-          </div>
         </div>
-
-        <div
-          id="tab-content3"
-          class="tab-content hidden w-full bg-gray-light overflow-hidden active:opacity-100"
-          aria-hidden="true"
-        >
+      </div>
+      <div class="tab-content3 hidden w-full bg-gray-lighter overflow-hidden">
+        <div class="my-10 mx-40 flex justify-center">
           <p>
             "Sed ut perspiciatis unde omnis iste natus error sit voluptatem
             accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
@@ -95,13 +57,7 @@ function App() {
             dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
             aspernatur aut odit aut fugit, sed quia consequuntur magni dolores
             eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est,
-            qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit,
-            sed quia non numquam eius modi tempora incidunt ut labore et dolore
-            magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis
-            nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut
-            aliquid ex ea commodi consequatur? Quis autem vel eum iure
-            reprehenderit qui in ea voluptate velit esse quam nihil molestiae
-            consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla?
+            qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit
           </p>
         </div>
       </div>
