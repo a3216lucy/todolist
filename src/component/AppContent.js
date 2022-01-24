4633f1bac06842b7e7916f5923a664a68dceda08
Kimi <lys.kimi@gms.tku.edu.tw>
2022-01-24 17:25:52 +0800

    Add: to get input data

--- src/component/AppContent.js
+++ src/component/AppContent.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
 import $ from 'jquery';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
@@ -8,72 +9,90 @@ import {
   faStar,
 } from '@fortawesome/free-regular-svg-icons';
 import { faPlusSquare, faPen } from '@fortawesome/free-solid-svg-icons';
+import { addTodo } from '../slices/todoSlice';
+import { v4 as uuid } from 'uuid';
 
 // import PageTitle from './PageTitle';
 
-class AppContent extends React.Component {
+// const date, time;
+function AppContent() {
+  // export default class AppContent extends React.Component {
   // constructor() {
   //   super();
-  //   this.focusTextInput = this.focusTextInput.bind(this);
   // }
 
-  componentDidMount() {
-    // this.focusTextInput();
-    $(function () {
-      $('.header , .body').hide();
-      $('#add-task').on('focus', function () {
-        $('#add-task').hide();
-        $('.pen').addClass('text-blue');
-        $('.header , .body ').show().slideDown().delay(3000);
-      });
-      $('.btn-cancel').on('click', function () {
-        $('.pen').removeClass('text-blue');
-        $('.header , .body ').hide();
-        $('#add-task').show();
-      });
-      $('.btn-submit').on('click', function () {
-        $('.pen').removeClass('text-blue');
-        $('.body ').hide();
-        $('#add-task').show();
-      });
-      $('.pen').on('click', function () {
-        $('.pen').addClass('text-blue');
-        $('.body').show().slideDown();
-      });
+  // componentDidMount(){
+  const [title, setTitle] = useState('');
+  const [date, setDate] = useState();
+  const [time, setTime] = useState();
+  const [comment, setComment] = useState('');
+  // const dispatch = useDispatch();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log({ title, date, time, comment});
+    // if (title && comment) {
+    //   dispatch(
+    //     addTodo({
+    //       id: uuid(),
+    //       title,
+    //       // status,
+    //       // date: new Date().toLocaleDateString,
+    //       // time: new Date().toLocaleTimeString,
+    //       comment,
+    //     })
+    //   );
+    // }
+  };
+
+  $(function () {
+    // $('.header , .body').hide();
+    $('#add-task').on('focus', function () {
+      $('#add-task').hide();
+      $('.pen').addClass('text-blue');
+      $('.header , .body ').show().slideDown().delay(3000);
     });
-  }
+    $('.btn-cancel').on('click', function () {
+      $('.pen').removeClass('text-blue');
+      $('.header , .body ').hide();
+      $('#add-task').show();
+    });
+    $('.btn-submit').on('click', function () {
+      $('.pen').removeClass('text-blue');
+      $('.body ').hide();
+      $('#add-task').show();
+    });
+    $('.pen').on('click', function () {
+      $('.pen').addClass('text-blue');
+      $('.body').show().slideDown();
+    });
+  });
+  // }
 
-  // focusTextInput() {
-  //   $(function () {
-  //         $('.body').hide();
-  //         $('#todo').on('focus', function () {
-  //           $('.body').show().slideDown();
-  //         });
-  //       });
-  //   }
+  // render(){
+  return (
+    <div class="w-96 max-w-screen-xl mx-auto">
+      {/* <PageTitle>This is app content.</PageTitle> */}
 
-  render() {
-    return (
-      <div class="w-96 max-w-screen-xl mx-auto">
-        {/* <PageTitle>This is app content.</PageTitle> */}
-
-        <div class="w-96 flex flex-col justify-center bg-white shadow-md rounded-sm mb-4">
-          <input
-            type="text"
-            id="add-task"
-            class="px-2 py-2 rounded-sm"
-            placeholder="＋ Add Task"
-          ></input>
-        </div>
+      <div class="w-96 flex flex-col justify-center bg-white shadow-md rounded-sm mb-4">
+        <input
+          type="text"
+          id="add-task"
+          class="px-2 py-2 rounded-sm"
+          placeholder="＋ Add Task"
+        ></input>
+      </div>
+      {/* <form class="" > */}
         <div class="header w-96 flex flex-col justify-center bg-gray-lighter shadow-md rounded-sm h-14 border-b border-gray">
           <div class="flex flex-row mx-3.5 my-1 ">
-            <input type="checkbox" id="checkbox" class="mx-2"></input>
+            <input type="checkbox" id="checkbox" class="mx-2" />
             <input
               type="text"
               id="todo"
               class="mx-0.5 bg-transparent"
               placeholder="Type Something Here..."
-              required
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
             ></input>
             <div class="ml-16 mx-2 cursor-pointer">
               <FontAwesomeIcon icon={faStar} />
@@ -99,6 +118,8 @@ class AppContent extends React.Component {
               id="date"
               class="ml-68 rounded-sm"
               placeholder="yyyy/mm/dd"
+              value={date}
+              onChange={(e) => setDate(e.target.value)}
             ></input>
 
             <input
@@ -106,6 +127,8 @@ class AppContent extends React.Component {
               id="time"
               class="mx-1.5 rounded-sm"
               placeholder="hh:mm"
+              value={time}
+              onChange={(e) => setTime(e.target.value)}
             ></input>
           </div>
           <div class="mt-3 mx-12">
@@ -131,15 +154,18 @@ class AppContent extends React.Component {
             id="comment"
             class="ml-68 px-2 pt-1 w-64 outline-none rounded-sm focus:outline-1 focus:outline-blue"
             placeholder="Type your memo here..."
+            value={comment}
+            onChange={(e) => setComment(e.target.value)}
           ></textarea>
           <div class="mt-6 shadow-md">
             <button class="w-1/2 btn-cancel">✕ Cancel</button>
-            <button class="w-1/2 btn-submit">＋ Add Task</button>
+            <button class="w-1/2 btn-submit" onClick={(e) => handleSubmit(e)}>＋ Add Task</button>
           </div>
         </div>
-      </div>
-    );
-  }
+      {/* </form> */}
+    </div>
+  );
+  // }
 }
 
 export default AppContent;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { store } from './app/store';
 import App from './App';
 import "./index.css";
 
 
 ReactDOM.render(
   <React.StrictMode>
+    <Provider store={store}></Provider>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
